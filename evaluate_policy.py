5de199240d9f37677e6b9dc458a21dad985b746c
Amelia Kuang <kuangzy.amelia@gmail.com>
2025-03-13 10:33:57 -0700

    add 329 checkpoint to evaluate_policy

--- evaluate_policy.py
+++ evaluate_policy.py
@@ -40,18 +40,35 @@ if __name__ == "__main__":
     sticky_action_p = 0.0
     env_name = "BreakoutNoFrameskip-v4"
     horizon = 400
+
+    # load policy
+    policy = BreakoutPolicy()
     # 105 on breakout with old env:
     # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250309_001316_skip4_sticky0.0_horizon400_optimSteps30_mem5/28.pkl"
     # 163 on breakout with new env:
-    policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_021201_skip4_sticky0.0_horizon300_optimSteps100_mem5/9.pkl"
+    # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_021201_skip4_sticky0.0_horizon300_optimSteps100_mem5/9.pkl"
     # policy_ckpt = None
+    # 203 on breakout with new env, trained with initial_policy_steps = 1000, initial_policy achieving 163 scores
+    # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_164217_skip4_sticky0.0_horizon300_optimSteps100_mem5/3.pkl"
+    # 329 on breakout with new env, trained with no initial policy stepping
+    policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_190211_skip4_sticky0.0_horizon300_optimSteps100_mem5/1.pkl"
+
+    from trained_policies.Breakout import Policy as InitialPolicy
+    # initial_policy = InitialPolicy()
+    # initial_policy_steps=1000
+    initial_policy = None
+    initial_policy_steps=None
+
+
     env = TracedEnv(render_mode="human",
                     env_name=env_name,
                     obs_mode="ori",
                     hud=False,
                     frameskip=frame_skip,
-                    repeat_action_probability=sticky_action_p)
-    policy = BreakoutPolicy()
+                    repeat_action_probability=sticky_action_p,
+                    initial_policy=initial_policy,
+                    initial_policy_steps=initial_policy_steps)
+
     if policy_ckpt:
         policy.load(policy_ckpt)
     for p in policy.parameters():
