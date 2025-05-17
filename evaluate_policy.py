b730f6a98f45a479f9ec31ce0c94bb5027e653fc
Amelia Kuang <kuangzy.amelia@gmail.com>
2025-05-17 15:18:14 -0700

    add evaluation script that loads checkpoints and run evals for three games

--- evaluate_policy.py
+++ evaluate_policy.py
@@ -1,79 +1,92 @@
 import gymnasium as gym
 import ale_py
 import numpy as np
+import argparse
+import os
 
 from dotenv import load_dotenv
-import opto.trace as trace
-from opto.trace import bundle, node, Module, GRAPH
-from pong_ocatari_LLM_agent import PongOCAtariTracedEnv, Policy as PongPolicy
-from breakout_ocatari_LLM_agent import TracedEnv, Policy as BreakoutPolicy
-
+from pong_ocatari_LLM_agent import PongOCAtariTracedEnv as PongEnv, Policy as PongPolicy
+from breakout_ocatari_LLM_agent import TracedEnv as BreakoutEnv, Policy as BreakoutPolicy
+from space_invaders_ocatari_LLM_agent import TracedEnv as SpaceInvadersEnv, Policy as SpaceInvadersPolicy
+    
 load_dotenv()
 gym.register_envs(ale_py)
 
-def test_policy(env, policy, num_episodes=10, steps_per_episode=4000):
-    rewards = []
+def test_policy(env, policy, steps_per_episode=4000):
+    obs, info = env.reset()
+    episode_reward = 0
+    steps = 0
     
-    for episode in range(num_episodes):
-        obs = env.reset()
-        episode_reward = 0
-        
-        for _ in range(steps_per_episode):
-            action = policy(obs)
-            obs, reward, terminated, truncated, _ = env.step(action)
-            episode_reward += reward
-            
-            if terminated or truncated:
-                break
-        
-        print(f"Episode {episode + 1} reward: {episode_reward}")
-        rewards.append(episode_reward)
-    
-    mean_reward = np.mean(rewards)
-    std_reward = np.std(rewards)
-    print(f"\nResults over {num_episodes} episodes:")
-    print(f"Mean reward: {mean_reward:.2f} ± {std_reward:.2f}")
-    return mean_reward, std_reward
+    for _ in range(steps_per_episode):
+        action = policy(obs)
+        obs, reward, terminated, truncated, _ = env.step(action)
+        episode_reward += reward
+        steps += 1
+        if terminated or truncated:
+            break
+ 
+    # print(f"\nResults over {num_episodes} episodes:")
+    print(f"Episode reward: {episode_reward} over {steps} steps")
+    return episode_reward
 
 if __name__ == "__main__":
-    frame_skip = 4
-    sticky_action_p = 0.0
-    env_name = "BreakoutNoFrameskip-v4"
-    horizon = 400
+    parser = argparse.ArgumentParser(description='Evaluate Policy Given Checkpoints')
+    parser.add_argument('--game', type=str, choices=['Pong', 'Breakout', 'SpaceInvaders'], 
+                    default='Pong', help='Atari game to evaluate, ["Pong", "Breakout", "SpaceInvaders"]')
+    parser.add_argument('--policy_ckpt_dir', type=str, required=True,
+                        help='Directory pointing to policy checkpoints')
+    parser.add_argument('--ckpt_iter', type=int, required=True,
+                        help='Checkpoint iteration number to load')
+    parser.add_argument('--render', action='store_true', help='Enable rendering')
 
-    # load policy
-    policy = BreakoutPolicy()
-    # 105 on breakout with old env:
-    # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250309_001316_skip4_sticky0.0_horizon400_optimSteps30_mem5/28.pkl"
-    # 163 on breakout with new env:
-    # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_021201_skip4_sticky0.0_horizon300_optimSteps100_mem5/9.pkl"
-    # policy_ckpt = None
-    # 203 on breakout with new env, trained with initial_policy_steps = 1000, initial_policy achieving 163 scores
-    # policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_164217_skip4_sticky0.0_horizon300_optimSteps100_mem5/3.pkl"
-    # 329 on breakout with new env, trained with no initial policy stepping
-    policy_ckpt = "trace_ckpt/BreakoutNoFrameskip-v4_20250312_190211_skip4_sticky0.0_horizon300_optimSteps100_mem5/1.pkl"
-
-    from trained_policies.Breakout import Policy as InitialPolicy
-    # initial_policy = InitialPolicy()
-    # initial_policy_steps=1000
-    initial_policy = None
-    initial_policy_steps=None
+    args = parser.parse_args()
 
 
-    env = TracedEnv(render_mode="human",
-                    env_name=env_name,
-                    obs_mode="ori",
-                    hud=False,
-                    frameskip=frame_skip,
-                    repeat_action_probability=sticky_action_p,
-                    initial_policy=initial_policy,
-                    initial_policy_steps=initial_policy_steps)
 
-    if policy_ckpt:
+    policy_ckpt = os.path.join(args.policy_ckpt_dir, f"{args.ckpt_iter}.pkl")
+    if not os.path.exists(policy_ckpt):
+        raise FileNotFoundError(f"Checkpoint not found: {policy_ckpt}")
+    render_mode = None
+    if args.render:
+        render_mode = "human"
+
+    obs_mode = "obj"
+    hud = False
+    frameskip = 4
+    repeat_action_probability = 0.00
+    if args.game == "Pong":
+        env = PongEnv(render_mode=render_mode,
+                                env_name="PongNoFrameskip-v4",
+                                obs_mode=obs_mode,
+                                hud=hud,
+                                frameskip=frameskip,
+                                repeat_action_probability=repeat_action_probability)
+        policy = PongPolicy()
         policy.load(policy_ckpt)
+    elif args.game == "Breakout":
+        env = BreakoutEnv(env_name="BreakoutNoFrameskip-v4",
+                          render_mode=render_mode,
+                          obs_mode=obs_mode,
+                          hud=hud,
+                          frameskip=frameskip,
+                          repeat_action_probability=repeat_action_probability,
+                          initial_policy=None,
+                          initial_policy_steps=None)
+        policy = BreakoutPolicy()
+        policy.load(policy_ckpt)
+    elif args.game == "SpaceInvaders":
+        env = SpaceInvadersEnv(env_name="SpaceInvadersNoFrameskip-v4",
+                               render_mode=render_mode,
+                               frameskip=frameskip,
+                               repeat_action_probability=repeat_action_probability)
+        policy = SpaceInvadersPolicy()
+        policy.load(policy_ckpt)
+    else:
+        raise ValueError(f"Invalid game name {args.game}")
+    
     for p in policy.parameters():
         print(p.name, p.data)
     try:
-        test_policy(env, policy, num_episodes=1)
+        test_policy(env, policy)
     finally:
         env.close()
